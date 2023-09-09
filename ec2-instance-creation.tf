2c3f4d25dbd236fd1a98e9b07575b01b961b9f3e
venkatasuresh <venkatasuresh@gmail.com>
2023-09-09 15:54:31 +0530

    update error in terraform

--- ec2-instance-creation.tf
+++ ec2-instance-creation.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 region = "ap-south-1"
 }
-resources "aws_instance" "server_machines"{
+resource "aws_instance" "server_machines"{
 ami = "ami-02bb7d8191b50f4bb"
 instance_type= "t2.micro"
 count=3
